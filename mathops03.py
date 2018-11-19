e9d837da6c9829150d6fd8e0c8f3b2bbb9b777ad
jonathan450 <44813750+jonathan450@users.noreply.github.com>
2018-11-19 10:23:49 -0700

    Add files via upload

---
+++ mathops03.py
@@ -0,0 +1,23 @@
+"Math ops and operands"
+
+a = 25
+b = 10
+c = 7
+
+print("Print the result of:",a,"+",b,"=",a + b)
+print("Print the result of:",a,"-",b,"=",a - b)
+print("Print the result of:",a,"*",b,"=",a * b)
+print("Print the result of:",a,"/",b,"=",a /b)
+
+# Integer Division //
+
+print("Print the result of:",a,"//",b,"=",a // b)
+print("Print the result of:",a,"//",c,"=",a // c)
+
+# Modules ( Remainder ) of an Integer Division
+
+print("Print the result of:",a,"%",b,"=",a % b)
+print("Print the result of:",c,"%",b,"=",c % b)
+
+#Power of a base raised to an exponent
+print("Find 2 raised at the 5th power:",2**5)
